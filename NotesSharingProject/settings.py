8f91881e8786eb5c1d8c787eb806581c3da0601b
Jyotimewada <jm2318600@gmail.com>
2025-05-06 16:28:34 +0530

    Update settings.py

--- NotesSharingProject/settings.py
+++ NotesSharingProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n3w0&qhhe(wnk_a7fb*n$#8^u)*3y*u&o2k#m(2_!n-1ogq^zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['website-8-ck3w.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
