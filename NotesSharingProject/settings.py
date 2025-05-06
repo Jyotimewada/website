c00ee6d170133efd899b5c215bc518178e41b176
Jyotimewada <jm2318600@gmail.com>
2025-05-06 16:34:50 +0530

    Update settings.py

--- NotesSharingProject/settings.py
+++ NotesSharingProject/settings.py
@@ -119,6 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Enable storage manifest for production to ensure cache-busting
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
